e818276e4dc1e66ff0eb155422334d31ba06d992
Maxi <maximuller11@gmail.com>
2022-02-20 17:30:27 -0300

    Public modifier removed from interface memebers

--- Runtime/FSM/IFSM.cs
+++ Runtime/FSM/IFSM.cs
@@ -4,10 +4,10 @@ namespace Padoru.Core
 {
 	public interface IFSM<TState, TTrigger> where TState : Enum where TTrigger : Enum
 	{
-		public void AddTransition(TState initialState, TState targetState, TTrigger trigger);
+		void AddTransition(TState initialState, TState targetState, TTrigger trigger);
 
-		public void SetTrigger(TTrigger trigger);
+		void SetTrigger(TTrigger trigger);
 
-		public State GetState(TState stateId);
+		State GetState(TState stateId);
 	}
 }
