91a6c7036f14b2e3d41a7090df9bb679b57d27ef
Maxi <maximuller11@gmail.com>
2022-02-20 16:40:07 -0300

    FSM Tests

---
+++ Tests/Editor/TestStates.cs
@@ -0,0 +1,9 @@
+namespace Padoru.Core.Tests
+{
+	public enum TestStates
+	{
+		State1 = 0,
+		State2 = 1,
+		State3 = 2,
+	}
+}
---
+++ Tests/Editor/TestTriggers.cs
@@ -0,0 +1,9 @@
+namespace Padoru.Core.Tests
+{
+	public enum TestTriggers
+	{
+		Trigger1 = 0,
+		Trigger2 = 1,
+		Trigger3 = 2,
+	}
+}
