7ee1262bc83bb279ef3e51f1dab6562eee62ca9f
Maxi <maximuller11@gmail.com>
2022-03-24 13:47:26 -0300

    Promises system

---
+++ Runtime/Promises/IPromise.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Padoru.Core
+{
+	public interface IPromise
+	{
+		IPromise OnComplete(Action action);
+		IPromise OnFail(Action<Exception> action);
+	}
+
+	public interface IPromise<T>
+	{
+		IPromise<T> OnComplete(Action<T> action);
+		IPromise<T> OnFail(Action<Exception> action);
+	}
+}
---
+++ Runtime/Promises/PromiseFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Padoru.Core
+{
+	public static class PromiseFactory
+	{
+		public static IPromise CreateCompleted()
+		{
+			var promise = new Promise();
+			promise.Complete();
+			return promise;
+		}
+
+		public static IPromise<T> CreateCompleted<T>(T result)
+		{
+			var promise = new Promise<T>();
+			promise.Complete(result);
+			return promise;
+		}
+
+		public static IPromise CreateFailed(Exception exception = null)
+		{
+			var promise = new Promise();
+			promise.Fail(exception);
+			return promise;
+		}
+
+		public static IPromise<T> CreateFailed<T>(Exception exception = null)
+		{
+			var promise = new Promise<T>();
+			promise.Fail(exception);
+			return promise;
+		}
+	}
+}
