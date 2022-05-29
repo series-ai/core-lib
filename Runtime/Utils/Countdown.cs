4ded40b2bd8973b7305e83f689d649634b432e35
Maxi <maximuller11@gmail.com>
2022-05-29 17:35:37 -0300

    Countdown class

---
+++ Runtime/Utils/Countdown.cs
@@ -0,0 +1,58 @@
+using System;
+
+using Debug = Padoru.Diagnostics.Debug;
+
+namespace Padoru.Core.Utils
+{
+	public class Countdown : ITickable
+	{
+		private ITickManager tickManager;
+		private float startTime;
+		public float currentTime;
+
+		public event Action<float> OnTimeChanged;
+		public event Action OnCountdownEnded;
+
+		public Countdown(float startTime)
+		{
+			this.startTime = startTime;
+
+			tickManager = Locator.GetService<ITickManager>();
+		}
+
+		~Countdown()
+		{
+			Stop();
+		}
+
+		public void Start()
+		{
+			Debug.Log($"Countdown started. Time: {startTime}");
+
+			currentTime = startTime;
+			tickManager.Register(this);
+		}
+
+		public void Stop()
+		{
+			Debug.Log("Countdown stopped");
+
+			tickManager.Unregister(this);
+		}
+
+		public void Tick(float deltaTime)
+		{
+			if(currentTime > 0)
+			{
+				currentTime -= deltaTime;
+				OnTimeChanged?.Invoke(currentTime);
+
+				if(currentTime <= 0)
+				{
+					OnCountdownEnded?.Invoke();
+					Stop();
+				}
+			}
+		}
+	}
+}
