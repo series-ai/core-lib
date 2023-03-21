84ec83d21ab2fdedd889f6079cd703d2fa5327a3
Luciano <luchopb.2@gmail.com>
2023-03-21 10:22:55 -0300

    Removed unused methods from test screen

--- Tests/Editor/UI/TestScreen.cs
+++ Tests/Editor/UI/TestScreen.cs
@@ -7,27 +7,7 @@
 
         }
 
-        public void Focus()
-        {
-
-        }
-
         public void Show()
-        {
-
-        }
-
-        public IPromise PlayIntroAnimation()
-        {
-            return PromiseFactory.CreateCompleted();
-        }
-
-        public IPromise PlayOutroAnimation()
-        {
-            return PromiseFactory.CreateCompleted();
-        }
-
-        public void Unfocus()
         {
 
         }
