64f1609ad38958b026fb638c754c17f1a43e8696
Federico Pardo <federico.pardo@dedalord.com>
2023-04-06 18:12:48 -0300

    Create SusbscribableValueJsonConverter

---
+++ Runtime/Utils/Values/SubscribableValueJsonConverter.cs
@@ -0,0 +1,20 @@
+using System;
+using Newtonsoft.Json;
+
+namespace Padoru.Core
+{
+    public class SubscribableValueJsonConverter<T> : JsonConverter<SubscribableValue<T>>
+    {
+        public override void WriteJson(JsonWriter writer, SubscribableValue<T> value, JsonSerializer serializer)
+        {
+            writer.WriteValue(value.Value);
+        }
+
+        public override SubscribableValue<T> ReadJson(JsonReader reader, Type objectType, SubscribableValue<T> existingValue, bool hasExistingValue,
+            JsonSerializer serializer)
+        {
+            var s = (T)reader.Value;
+            return new SubscribableValue<T>(s);
+        }
+    }
+}
