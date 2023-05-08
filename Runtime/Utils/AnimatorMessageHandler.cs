bdd45d39f096ccbed49f9bd3dad0a958fe12a537
Maxi Muller <maximuller11@gmail.com>
2023-05-08 09:22:43 -0300

    AnimatorMessageHandler added to the project

---
+++ Runtime/Utils/AnimatorMessageHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AnimatorMessageHandler : MonoBehaviour
+{
+    private Dictionary<string, UnityEvent> events = new ();
+
+    public UnityEvent GetEvent(string eventName)
+    {
+        eventName = eventName.ToLower();
+
+        if (!events.ContainsKey(eventName))
+        {
+            events.Add(eventName, new UnityEvent());
+        }
+        
+        return events[eventName];
+    }
+
+    public void ClearEvents()
+    {
+        events.Clear();
+    }
+
+    public void TriggerEvent(string eventName)
+    {
+        GetEvent(eventName).Invoke();
+    }
+}
