f35c55dba380bccdf519c545360a5be2a72fb288
Nicolas Florido <nico.florido@hotmail.com>
2023-10-24 09:56:05 -0300

    Fixed web file system

--- Runtime/FilesManagement/FileSystems/WebFileSystem.cs
+++ Runtime/FilesManagement/FileSystems/WebFileSystem.cs
@@ -8,15 +8,17 @@ namespace Padoru.Core.Files
     public class WebFileSystem : IFileSystem
     {
         private readonly string basePath;
+        private readonly string protocol;
 
-        public WebFileSystem(string basePath)
+        public WebFileSystem(string basePath, string protocol)
         {
             this.basePath = basePath;
+            this.protocol = protocol;
         }
         
         public async Task<bool> Exists(string uri)
         {
-            var path = GetFullPath(uri);
+            var path = GetAppropiateUri(uri);
             
             var uwr = UnityWebRequest.Get(path);
             var request = uwr.SendWebRequest();
@@ -31,7 +33,7 @@ namespace Padoru.Core.Files
 
         public async Task<File<byte[]>> Read(string uri)
         {
-            var path = GetFullPath(uri);
+            var path = GetAppropiateUri(uri);
             
             var uwr = UnityWebRequest.Get(path);
             var request = uwr.SendWebRequest();
@@ -55,53 +57,39 @@ namespace Padoru.Core.Files
         public async Task Write(File<byte[]> file)
         {
             var path = GetFullPath(file.Uri);
-            
-            var uwr = new UnityWebRequest(path, UnityWebRequest.kHttpVerbPOST);
-            var myUploadHandler = new UploadHandlerRaw(file.Data);
-            myUploadHandler.contentType= "application/x-www-form-urlencoded"; // might work with 'multipart/form-data'
-            uwr.uploadHandler= myUploadHandler;
-            
-            var request = uwr.SendWebRequest();
 
-            while (!request.isDone)
-            {
-                await Task.Yield();
-            }
+            var directory = Path.GetDirectoryName(path) ?? ".";
             
-            if (uwr.result == UnityWebRequest.Result.Success)
-            {
-                Debug.Log($"Written file at path '{path}'.");
-            }
-            else
-            {
-                Debug.LogError($"Could not write file at path '{path}'. Error: {uwr.error}");
-            }
+            Directory.CreateDirectory(directory);
+
+            await File.WriteAllBytesAsync(path, file.Data);
+
+            Debug.Log($"Wrote file to path '{path}'");
         }
 
         public async Task Delete(string uri)
         {
             var path = GetFullPath(uri);
-            
-            var uwr = UnityWebRequest.Delete(path);
-            var request = uwr.SendWebRequest();
 
-            while (!request.isDone)
+            if (!File.Exists(path))
             {
-                await Task.Yield();
+                throw new FileNotFoundException($"Could not find file. Uri {uri}");
             }
             
-            if (uwr.result == UnityWebRequest.Result.Success)
-            {
-                Debug.Log($"Deleted file at path '{path}'.");
-                return;
-            }
-
-            throw new FileNotFoundException($"Could not find file. Uri {uri}. Error: {uwr.error}");
+            File.Delete(path);
+            
+            await Task.CompletedTask;
         }
         
         private string GetFullPath(string uri)
         {
             return Path.Combine(basePath, FileUtils.ValidatedFileName(FileUtils.PathFromUri(uri)));
+        }
+
+        private string GetAppropiateUri(string uri)
+        {
+            var path = GetFullPath(uri);
+            return protocol + path;
         }
     }
 }
