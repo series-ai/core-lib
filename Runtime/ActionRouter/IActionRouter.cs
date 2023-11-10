9389add4cef3ea35e202423820aa13ff9cd0068f
Maxi Muller <maximuller11@gmail.com>
2023-11-10 12:01:08 -0300

    Added missing method to the IActionRouter interface

--- Runtime/ActionRouter/IActionRouter.cs
+++ Runtime/ActionRouter/IActionRouter.cs
@@ -6,6 +6,7 @@ namespace Padoru.Core.ActionRouter
     {
         void Subscribe(string actionId, Action<object> subscriber); 
         void Unsubscribe(string actionId, Action<object> subscriber);
+		bool IsActionSubscribed(string actionId);
         void Invoke(string actionId, object actionObject);
     }
 }
