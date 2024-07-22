eadd1cb1f0b94769d05b528d62e3378d58348469
Maxi Muller <maximuller11@gmail.com>
2024-07-22 18:52:00 -0300

    Added partial debug channels class

---
+++ Runtime/Utils/DebugChannels.cs
@@ -0,0 +1,8 @@
+public static partial class DebugChannels
+{
+    public const string PADORU = "Padoru";
+    public const string DEFAULT = "Default";
+    public const string APP_LIFE_CYCLE = "AppLifeCycle";
+    public const string COMMANDS_CONSOLE = "CommandsConsole";
+    public const string GAME_FSM = "GameFSM";
+}
